7b8fad6a99bfb06d08c0d6e9aa5e15ead1bbd11a
joshuamshana <joshua.mshana@nmbtz.com>
2026-05-18 00:06:14 +0300

    Remove IDE configuration files and update main entry point

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import week_4.task_1.AuthorBookApp;
+import week_4.task_2.AccountCustomerApp;
 
 
 public class Main {
     public static void main(String[] args) {
-        new AuthorBookApp();
+//        new AuthorBookApp();
+        new AccountCustomerApp();
     }
 }
